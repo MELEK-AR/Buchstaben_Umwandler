a7076c25043d82bb9b986d3a2975563c840cd26c
MELEK-AR <115654788+MELEK-AR@users.noreply.github.com>
2025-01-23 21:15:33 +0100

    Add files via upload

---
+++ Model.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grossbuchstaben_MVVM
+{
+    internal class Model
+    {
+        public Model() { }
+
+
+        public string NachGross(string klein)
+        {
+            string gross = null;
+
+            gross = klein.ToUpper();
+            return gross;
+        }
+        public string NachKlein(string gross)
+        {
+            string klein = null;
+
+            klein = gross.ToLower();
+            return klein;
+        }
+
+    }
+}
---
+++ ViewModel.cs
@@ -0,0 +1,41 @@
+﻿using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grossbuchstaben_MVVM
+{
+    class ViewModel: BindableBase
+    {
+        private string kleinwort;
+        private string grosswort;
+        private Model model;
+
+        public string Kleinwort { get { return kleinwort; } set { SetProperty(ref kleinwort, value); } } 
+        public string Grosswort { get {return grosswort; } set { SetProperty(ref grosswort, value); } }
+
+        public ViewModel()
+        {
+            model = new Model();
+        }
+
+        public void Umwandeln()
+        {
+            if (!string.IsNullOrEmpty(kleinwort))
+            {
+                Grosswort = model.NachGross(kleinwort);
+            }
+            else if(!string.IsNullOrEmpty(grosswort)){ 
+                Kleinwort = model.NachKlein(grosswort);
+            }
+        }
+
+        public void Reset()
+        {
+            Grosswort = "";
+            Kleinwort = "";
+        }
+    }
+}
